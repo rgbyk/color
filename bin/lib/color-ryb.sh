8834dd35005eeef5517926a73d09c049e0face29
Daniel Haim <2808046+danielhaim1@users.noreply.github.com>
2023-01-08 21:52:50 +0100

    v1.4.9

--- bin/lib/color-ryb.sh
+++ bin/lib/color-ryb.sh
@@ -15,7 +15,7 @@ set -e
 echo $white_ "... $ color.sh;" $reset
 
 echo $black_ "... $ sass $FILE_STYLE_RYB;" $reset
-sass $FOLDER_SRC_SCSS/$FILE_STYLE_RYB.scss $FOLDER_DIST_ASSETS_CSS/$FILE_STYLE_RYB.css --no-source-map --charset --style=compressed
+sass $FOLDER_SRC_SCSS/$FILE_STYLE_RYB.scss $FOLDER_DIST_ASSETS_CSS/$FILE_STYLE_RYB.css --no-source-map --charset
 
 echo $black_ "... $ postcss $FILE_STYLE_RYB;" $reset
 postcss $FOLDER_DIST_ASSETS_CSS/$FILE_STYLE_RYB.css -o $FOLDER_DIST_ASSETS_CSS/$FILE_STYLE_RYB.min.css
